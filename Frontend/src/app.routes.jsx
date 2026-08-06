4ed222316c94d9f0c5f3126ab8a56c593a046011
Priyanshu Kumar <priyanshukumar.stu.87@gmail.com>
2026-08-06 16:33:53 +0530

    correct file path in app.routes.jsx

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,9 +1,9 @@
 import {createBrowserRouter} from "react-router";
-import Login from "./features/auth/pages/login";
-import Register from "./features/auth/pages/Register";
-import Protected from "./features/auth/components/protected";
-import Home from "./features/interview/pages/Home";
-import Interview from "./features/interview/pages/Interview";
+import Login from "./features/auth/pages/login.jsx";
+import Register from "./features/auth/pages/Register.jsx";
+import Protected from "./features/auth/components/protected.jsx";
+import Home from "./features/interview/pages/Home.jsx";
+import Interview from "./features/interview/pages/Interview.jsx";
 
 
 const routes = createBrowserRouter([
